0a5663aa6c92c6a5fbd0764723eaffca6c5acbdd
John <tve24el033@cet.ac.in>
2026-04-09 18:36:08 +0530

    c3

--- src/BooleanMatrix.java
+++ src/BooleanMatrix.java
@@ -1,38 +1,46 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class BooleanMatrix 
-{
-    public static void main(String[] args)
-        {
-                Scanner sc = new Scanner(System.in);
-                int m = sc.nextInt();
-                int n = sc.nextInt();
-                int arr[][] = new int[m][n];
-                for (int i=0;i<m;i++)
-                        for (int j=0;j<n;j++)
-                                arr[i][j] = sc.nextInt();
-                for (int i=0;i<m;i++)
-                {
-                        boolean flag=false;
-                        for (int j=0;j<n;j++)
-                        {
-                                if (arr[i][j] == 1)
-                                {
-                                        flag = true;
-                                        break;
-                                }
-                        }
-                        if(flag)
-                        {
-                                for (int j=0;j<n;j++)
-                                        arr[i][j]=1;
-                        }
+public class BooleanMatrix {
+
+    public static void BooleanMatrixProblem(int m, int n, int arr[][]){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] == 1) {
+                    for (int k = 0; k < n; k++) {
+                        arr[i][k] = 1;
+                    }
+                    break;
                 }
-                for (int i=0;i<n;i++)
-                {
-                        for (int j=0;j<n;j++)
-                                System.out.print(arr[i][j]+" ");
-                        System.out.println();
-                }
+            }
         }
+    }
+
+    public static void print(int m, int n, int arr[][]){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j]);
+                if (j < n - 1) System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int arr[][] = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        BooleanMatrixProblem(m, n, arr);
+        print(m, n, arr);
+
+        sc.close();
+    }
 }
